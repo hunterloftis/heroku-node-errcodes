4e4b95c42f000e8d7c126e0f7b05fccdb43ddc0f
Hunter Loftis <hunter@hunterloftis.com>
2016-04-21 15:16:58 -0400

    replace server with h18

--- h18/h18.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const PORT = process.env.PORT || 3000;
-
-http.createServer((req, res) => {
-    res.write('test', 'utf8', () => {
-        req.socket.destroy();
-    });
-}).listen(PORT);
